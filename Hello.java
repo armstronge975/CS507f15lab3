05ad0a09ebc7494897bd0ac453aae8ded3b5be91
armstronge975 <armstronge975@strose.edu>
2015-10-04 16:26:44 -0400

    Making sure everything works

--- Hello.java
+++ Hello.java
@@ -7,4 +7,5 @@ public class Hello {
 public static void main(String[] args) {
   System.out.println("Hey Emily :)");
   System.out.println("Emily's Commit");
+  System.out.println("My favorite food is 1011111011101111");
 }
